e8457a34a9d521d6b0f569acb1997392e6605690
Laxenarde <alexandre.lebourch@ynov.com>
2023-11-07 11:20:03 +0100

    add tests for sow and water

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -40,26 +40,40 @@ def test_needed_water_4():
     assert field.needed_water()
 
 
-def can_harvest_sell_1():
+def test_can_harvest_sell_1():
     field = Field(Location.FIELD1)
     assert not field.can_harvest_sell()
 
 
-def can_harvest_sell_2():
+def test_can_harvest_sell_2():
     field = Field(Location.FIELD1)
     field.content = Aliment.POTATO
     field.water_lvl = 0
     assert field.can_harvest_sell()
 
 
-def can_harvest_sell_3():
+def test_can_harvest_sell_3():
     field = Field(Location.FIELD1)
     field.content = Aliment.POTATO
     field.water_lvl = 5
     assert not field.can_harvest_sell()
 
 
-def can_harvest_sell_4():
+def test_can_harvest_sell_4():
     field = Field(Location.FIELD1)
     field.content = Aliment.NONE
     assert not field.can_harvest_sell()
+
+
+def test_sow():
+    field = Field(Location.FIELD1)
+    field.sow()
+    assert not field.is_sowable()
+
+
+def test_water():
+    field = Field(Location.FIELD1)
+    field.sow(Aliment.POTATO)
+    field.water_lvl = 5
+    field.water()
+    assert field.water_lvl == 4
